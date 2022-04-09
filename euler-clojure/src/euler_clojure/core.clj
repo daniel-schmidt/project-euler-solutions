f857c2c1868f730c99714add15d4d433a337bf7f
Daniel Schmidt <daniel@neonfoto.de>
2022-04-09 22:29:54 +0200

    Clojure implementation filtering a range and reducing it.

--- euler-clojure/src/euler_clojure/core.clj
+++ euler-clojure/src/euler_clojure/core.clj
@@ -2,15 +2,27 @@
   (:gen-class))
 
 (defn prob_1_precalculated
-  "Solution of first problem by analytical calculation using the Gaussian sum formula."
+  "Problem 1: precalculated solution using the Gaussian sum formula"
   []
   (+ (* 3 333 167)
      (* 5 199 100)
      (* -15 33 67)))
 
 
+(defn prob_1_filtered
+  "Problem 1: filter a range and reduce it"
+  []
+  (def div_by_3_or_5?
+    (fn [x]
+      (or (== (mod x 3) 0)
+          (== (mod x 5) 0))))
+  (reduce + (filter div_by_3_or_5? (range 1000))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Solution to problem 1:")
-  (println (prob_1_precalculated)))
+  (println (prob_1_precalculated))
+  (println (prob_1_filtered)))
+
