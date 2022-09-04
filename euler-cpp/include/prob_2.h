f52be4673ace52d157eee2bc34eb5cf5983322bb
Daniel Schmidt <daniel@neonfoto.de>
2022-09-04 22:53:44 +0200

    Provide a more direct implementation on the c++ side which returns the same results as the clojure one.

--- euler-cpp/include/prob_2.h
+++ euler-cpp/include/prob_2.h
@@ -5,4 +5,6 @@ namespace prob_2
 // Calculate the sum of all even Fibonacci numbers not exceeding the given threshold.
 
 int sum_of_even_fib_below(int threshold);
+
+int clever_sum_of_even_fib_below(int threshold);
 }
--- euler-cpp/src/prob_2.cpp
+++ euler-cpp/src/prob_2.cpp
@@ -6,6 +6,23 @@
 namespace prob_2
 {   
 
+int sum_of_even_fib_below(int threshold)
+{
+    int f_2 = 1;        // Fibonacci number n-2
+    int f_1 = 2;        // Fibonacci number n-1
+    int sum = 2;
+    do {
+        int f_curr = f_1 + f_2; // Fibonacci number n-1
+        if(f_curr % 2 == 0) {
+            sum += f_curr;
+        }
+        f_2 = f_1;
+        f_1 = f_curr;
+    } while(f_1 <= threshold);
+    return sum;
+}
+
+
 // Calculate the nth fibonacci number using the formula of Moivre-Binet.
 int fib_moivre_binet(int n)
 {
@@ -16,7 +33,7 @@ int fib_moivre_binet(int n)
     return inv_sqrt_5 * (std::pow(phi, n) - std::pow(psi, n));    
 }
 
-int sum_of_even_fib_below(int threshold)
+int clever_sum_of_even_fib_below(int threshold)
 {
     int sum = 0;
     int idx = 3;
